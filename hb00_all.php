3fe1107e65f2027673e90d7436e5cbb1bea870d7
roblindh <roblindh@gmail.com>
2026-06-01 15:46:37 +0200

    Add PDF/Print button to hb00_all.php and optimize print styles

--- hb00_all.php
+++ hb00_all.php
@@ -20,6 +20,14 @@
                 <?php echo rol_toc(102); ?>
             </nav>
             <section>
+                <div class="print-header no-print">
+                    <button onclick="window.print()" class="btn-print">
+                        <svg class="icon-print" viewBox="0 0 24 24" width="16" height="16">
+                            <path fill="currentColor" d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"/>
+                        </svg>
+                        Print / Save as PDF
+                    </button>
+                </div>
                 <?php
                 set_time_limit(30 * 60);
                 include 'hb01_intro_content.php';
