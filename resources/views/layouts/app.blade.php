583c53bf536d89fd04cf99445ba6dc2e422e4f6e
roblindh <roblindh@gmail.com>
2026-08-31 06:28:21 +0200

    feat(branding): add project logo favicon and app icons for browser tab display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,12 @@
     <!-- Compiled Tailwind CSS & Site Styling -->
     <link rel="stylesheet" href="/styles/tailwind.min.css">
     <link rel="stylesheet" href="/styles/Site.css">
-    <link rel="icon" href="/styles/reddragon.ico">
+    <!-- Favicon & Browser Icons -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 
     <!-- Local Alpine.js -->
     <script defer src="/js/alpine.min.js"></script>
